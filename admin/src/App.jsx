b8696d34b9c86470028219860194b24e20016357
Ushabanu <ushabanut@gmail.com>
2025-04-11 16:51:31 +0530

    Your commit message

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Login from './pages/Login.jsx'
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import { AdminContext } from './context/AdminContext.jsx';
+import Navbar from './components/Navbar.jsx';
 
 const App = () => {
-  return (
-    <div>
-      <Login />
+
+  const {aToken} = useContext(AdminContext)
+
+  return aToken ? (
+    <div className='bg-[#F8F9FD]'>
+      <ToastContainer />
+      <Navbar/>
     </div>
+  ) : (
+    <>
+    <Login />
+    <ToastContainer />
+    </>
   )
 }
 
--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,72 +1,100 @@
-import validator from 'validator'
-import bcrypt from 'bcrypt'
-import doctorModel from '../models/doctorModel.js'
-import {v2 as cloudinary} from 'cloudinary'
-import jwt from 'jsonwebtoken'
+import validator from "validator";
+import bcrypt from "bcrypt";
+import doctorModel from "../models/doctorModel.js";
+import { v2 as cloudinary } from "cloudinary";
+import jwt from "jsonwebtoken";
 
 // API for adding doctor
-const addDoctor = async (req,res) => {
+const addDoctor = async (req, res) => {
+  try {
+    const {
+      name,
+      email,
+      password,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address,
+    } = req.body;
+    const imageFile = req.file;
 
-    try {
-        const{name, email, password, speciality, degree, experience, about, fees, address} = req.body
-        const imageFile = req.file
-        
-        if(!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address){
-            return res.json({success:false, message:'Missing Details'})
-        }
-
-        if(!validator.isEmail(email)) return res.json({success:false, message:'Please enter a valid email'})
-    
-
-        if(password.length<8) return res.json({success:false, message:'Please enter a strong password'})
-
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password, salt)   
-        
-        const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type:'image'}) 
-        const imageUrl = imageUpload.secure_url
-
-        const doctorData = {
-            name,
-            email,
-            image:imageUrl,
-            password:hashedPassword,
-            speciality,
-            degree,
-            experience,
-            about,
-            fees,
-            address:JSON.parse(address),
-            date:Date.now()
-        }
-
-        const newDoctor = new doctorModel(doctorData)
-        await newDoctor.save()
-
-        res.json({success:true, message:'Doctor Added'})
-
-    } catch (error) {
-        console.log(error);
-        res.json({success:false, message:error.message})
+    if (
+      !name ||
+      !email ||
+      !password ||
+      !speciality ||
+      !degree ||
+      !experience ||
+      !about ||
+      !fees ||
+      !address
+    ) {
+      return res.json({ success: false, message: "Missing Details" });
     }
 
-}
+    if (!validator.isEmail(email))
+      return res.json({
+        success: false,
+        message: "Please enter a valid email",
+      });
 
-const loginAdmin = async(req,res)=>{
-    try {
-        const{email,password} = req.body
+    if (password.length < 8)
+      return res.json({
+        success: false,
+        message: "Please enter a strong password",
+      });
 
-        if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
-            const token = jwt.sign(email+password, process.env.JWT_SECRET)
-            res.json({success:true, token})
-        }else{
-            res.json({success:true, message:'Invalid Credentials'})
-        }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-    } catch (error) {
-        console.log(error);
-        res.json({success:false, message:error.message})
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+      resource_type: "image",
+    });
+    const imageUrl = imageUpload.secure_url;
+
+    const doctorData = {
+      name,
+      email,
+      image: imageUrl,
+      password: hashedPassword,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address: JSON.parse(address),
+      date: Date.now(),
+    };
+
+    const newDoctor = new doctorModel(doctorData);
+    await newDoctor.save();
+
+    res.json({ success: true, message: "Doctor Added" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+const loginAdmin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid Credentials" });
     }
-}
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
-export {addDoctor,loginAdmin}
+export { addDoctor, loginAdmin };
